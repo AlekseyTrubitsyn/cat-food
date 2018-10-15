4499f2cce5c03eaff09b86e3b57b60d5e7201c28
Aleksey Trubitsyn <at.8441451@yandex.ru>
2018-10-15 21:22:05 +0300

    build

---
+++ build/precache-manifest.83261a2b5ffac71e22ba05b9b257bdd6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "0733ed01dc697b74b02f",
+    "url": "./static/js/main.0733ed01.chunk.js"
+  },
+  {
+    "revision": "3ef4ad9fe1845d519ea5",
+    "url": "./static/js/1.3ef4ad9f.chunk.js"
+  },
+  {
+    "revision": "2a7fd107c021b21b839362d9d3b2d4e4",
+    "url": "./index.html"
+  }
+];
